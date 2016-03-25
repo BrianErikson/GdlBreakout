1c9b2e605988313cd5e6c326872543307ae3d4ad
pilly101 <nphelpsiv@gmail.com>
2016-03-25 16:54:28 -0600

    Bricks are prefabs have health: #4 #6

---
+++ Assets/Source/BrickBehavior.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrickBehavior : MonoBehaviour {
+    public int health;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Ball")
+        {
+            health--;
+            if(health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
