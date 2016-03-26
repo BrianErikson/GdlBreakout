d6115ec39fb081bf0b7b495c27a429bc944794ba
pilly101 <nphelpsiv@gmail.com>
2016-03-25 18:08:54 -0600

    Gameover text and death on ball: #5

---
+++ Assets/Source/BottomBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BottomBehavior : MonoBehaviour {
+    GameManager gm;
+	// Use this for initialization
+	void Start () {
+        gm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Ball")
+        {
+            Destroy(coll.gameObject);
+            gm.EndGame();
+        }
+    }
+}
---
+++ Assets/Source/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+    Text gameOverText;
+    public float gameOverTimer;
+    private float passedTime;
+    private bool startTimer = false;
+    // Use this for initialization
+    void Start () {
+        gameOverText = GameObject.FindGameObjectWithTag("GameOverText").GetComponent<Text>();
+        gameOverText.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(startTimer == true)
+        {
+            passedTime += Time.deltaTime;
+            if(passedTime > gameOverTimer)
+            {
+                RestartLevel();
+            }
+        }
+	}
+    public void StartLevel()
+    {
+
+    }
+    public void RestartLevel()
+    {
+        Application.LoadLevel("Game");
+    }
+    public void EndGame()
+    {
+        gameOverText.enabled = true;
+        startTimer = true;
+    }
+    public void NextLevel()
+    {
+
+    }
+}
