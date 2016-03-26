6e5ff43a53fb13deb20ad70e571ac28eab54b867
BrianErikson <briankerikson@gmail.com>
2016-03-26 15:35:59 +0100

    completed brick health color #8

--- Assets/Source/BrickBehavior.cs
+++ Assets/Source/BrickBehavior.cs
@@ -2,16 +2,29 @@
 using System.Collections;
 
 public class BrickBehavior : MonoBehaviour {
+    public Sprite[] sprites;
+
+    [Tooltip("Cannot be a higher value than the length of the array of sprites")]
     public int health;
+
+    private SpriteRenderer spriteRenderer;
+
 	// Use this for initialization
 	void Start () {
-	
+        if (health > sprites.Length)
+        {
+            health = sprites.Length;
+        }
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        UpdateSprite();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
     void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.gameObject.tag == "Ball")
@@ -21,6 +34,18 @@ public class BrickBehavior : MonoBehaviour {
             {
                 Destroy(gameObject);
             }
+            else
+            {
+                UpdateSprite();
+            }
+        }
+    }
+
+    private void UpdateSprite()
+    {
+        if (health >= 0 && health <= sprites.Length)
+        {
+            spriteRenderer.sprite = sprites[health - 1];
         }
     }
 }
